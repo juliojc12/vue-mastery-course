b915c77eb68f888f5d29bae7a1f1f80b87910ae7
Julio Cesar <juliojc12@gmail.com>
2022-11-26 18:47:05 -0300

    lesson 8, computed properties

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Meias',
-            image: './assets/images/socks_green.jpg',
-            url: 'https://github.com/juliojc12',
-            inStock: true,
+            brand: 'Vue Mastery',
+            selectVariant: 0,
             details: ['50% algodão', '30% poliéster', '20% nylon'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ]
         }
 
@@ -22,8 +21,19 @@ const app = Vue.createApp({
             if (this.cart > 0)
                 this.cart -= 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        upadateVariant(index) {
+            this.selectVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.product + ' ' + this.brand
+        },
+        image() {
+            return this.variants[this.selectVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectVariant].quantity
         }
     }
 })
